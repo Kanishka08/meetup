83fea6f6ba20d780048b6c317c46434bddd4e7d6
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-06-03 21:16:46 +0530

    Prerendering the powers of Next JS

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import MeetupList from "../components/meetups/MeetupList";
 
-const Dummy_MEETUPS = [
+const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "A First Meetup",
@@ -18,7 +18,30 @@ const Dummy_MEETUPS = [
     description: "This is Second Meetup!",
   },
 ];
-function HomePage() {
-  return <MeetupList meetups={Dummy_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+//   export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
 }
 export default HomePage;
