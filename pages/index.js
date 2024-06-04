bd51d502ceea604b7fa6c1574999af0b7a83aef8
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-06-04 21:38:14 +0530

    SEO Friendly

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,21 @@
+import { Fragment } from "react";
+import Head from "next/head";
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 5, 12345 Some city",
-    description: "This is First Meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 45, 1234 India",
-    description: "This is Second Meetup!",
-  },
-];
 function HomePage(props) {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
 }
 
 //   export async function getServerSideProps(context) {
@@ -37,11 +33,28 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://kanishka2222:8yXUovOrkBGgT37h@atlascluster.i04w0.mongodb.net/meetups?retryWrites=true&w=majority&appName=AtlasCluster"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
 }
+
 export default HomePage;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,39 @@
 //our-domain.com/new-meetup
+import { Fragment } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
 import NewMeetupFrom from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
   }
 
-  return <NewMeetupFrom onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupFrom onAddMeetup={addMeetupHandler} />;
+    </Fragment>
+  );
 }
 export default NewMeetupPage;
